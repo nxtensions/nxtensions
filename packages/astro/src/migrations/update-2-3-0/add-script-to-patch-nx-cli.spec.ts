19c464924c404a86dc1cc15d1cd6c63ff7b26d0f
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-06-25 17:45:27 +0100

    feat(astro): add migration to generate script to patch the nx cli for esm module import support

---
+++ packages/astro/src/migrations/update-2-3-0/add-script-to-patch-nx-cli.spec.ts
@@ -0,0 +1,39 @@
+import type { Tree } from '@nrwl/devkit';
+import { readJson, updateJson } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import migration from './add-script-to-patch-nx-cli';
+
+describe('Test Suite Name', () => {
+  let tree: Tree;
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace(2);
+  });
+
+  test('should generate script to patch the nx cli', async () => {
+    await migration(tree);
+
+    expect(tree.exists('tools/scripts/patch-nx-cli.js')).toBe(true);
+  });
+
+  test('should add a postinstall script to patch the nx cli', async () => {
+    await migration(tree);
+
+    const { scripts } = readJson(tree, 'package.json');
+    expect(scripts.postinstall).toBe('node ./tools/scripts/patch-nx-cli.js');
+  });
+
+  test('should update the postinstall script to patch the nx cli', async () => {
+    updateJson(tree, 'package.json', (json) => ({
+      ...json,
+      scripts: { postinstall: 'node ./some-important-script.js' },
+    }));
+
+    await migration(tree);
+
+    const { scripts } = readJson(tree, 'package.json');
+    expect(scripts.postinstall).toBe(
+      'node ./some-important-script.js && node ./tools/scripts/patch-nx-cli.js'
+    );
+  });
+});
