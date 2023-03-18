9bdfd493545ef989cab2aa7aaedfd44670c112f8
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-03-18 13:04:54 +0000

    chore(repo): bump deps

--- e2e/astro/jest.config.js
+++ e2e/astro/jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   displayName: 'e2e-astro',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/e2e/astro',
