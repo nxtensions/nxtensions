a9d083e3f1a015e32f52de726de99a93d37e58a0
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-08-06 12:06:04 +0100

    fix(astro): generate correct tailwind integration config (#453)

--- packages/astro/src/generators/application/utilities/index.ts
+++ packages/astro/src/generators/application/utilities/index.ts
@@ -1,6 +1,7 @@
 export * from './add-files';
 export * from './add-project';
 export * from './add-integrations-packages';
+export * from './configure-tailwind-integration';
 export * from './normalize-options';
 export * from './set-default-project';
 export * from './setup-e2e-tests';
