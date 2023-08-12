963168dd352ad0eb33e865ee46e420647f1da263
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-08-12 08:35:20 +0100

    feat(astro): restore usage of astro's latest version

--- packages/astro/src/generators/init/versions.ts
+++ packages/astro/src/generators/init/versions.ts
@@ -1,3 +1,1 @@
-// TODO(leo): temporary pin version to 2.9.6 (last working version) until
-// https://github.com/withastro/astro/issues/7934 is addressed.
-export const astroVersion = '2.9.6';
+export const astroVersion = '^2.10.7';
