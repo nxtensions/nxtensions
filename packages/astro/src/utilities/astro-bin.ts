07b415b2c0a9b6445055d60e50d150fa824762cc
Martinez Echeverria, Miguel <miguel.martinez@gft.com>
2023-09-28 18:14:08 +0200

    fix(astro): delete unnecessary file

--- packages/astro/src/utilities/astro-bin.ts
+++
@@ -1,6 +0,0 @@
-import { workspaceRoot } from '@nx/devkit';
-import { normalize, join } from 'path';
-
-export function getAstroBinPath(): string {
-  return normalize(join(workspaceRoot, 'node_modules', '.bin', 'astro'));
-}
